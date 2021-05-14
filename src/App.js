855424686a7aad881d6bb45ce82c7edf7254a5dd
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-05-14 15:28:52 +0900

    complete-areaの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { InputArea } from './components/InputArea'
+import { CompleteArea } from './components/CompleteArea'
+
 
 function App() {
   return (
     <>
+      <h1>TODO List</h1>
       <InputArea></InputArea>
+      <CompleteArea></CompleteArea>
     </>
   );
 }
---
+++ src/components/CompleteArea.jsx
@@ -0,0 +1,19 @@
+export const CompleteArea = () => {
+  return (
+    <div className="complete-area">
+      <h2>Complete List</h2>
+      <ul>
+        <li className="list-items">
+          make lunch
+          <button>Done</button>
+          <button>Delete</button>
+        </li>
+        <li>
+          take a picture
+          <button>Done</button>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
